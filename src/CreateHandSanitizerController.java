b7bcce9039b03425cbec7c029982f4634ad4d7a9
Jaret Wright <hkkA223>
2020-09-23 13:06:39 -0400

    Initial commit

---
+++ src/CreateHandSanitizerController.java
@@ -0,0 +1,11 @@
+public class CreateHandSanitizerController {
+
+    /**
+     * Print something to the console
+     *
+     */
+    public void submitButtonClicked()
+    {
+        System.out.println("Hello World from GUI");
+    }
+}
